2a0fffa2153cd6897454fe9b8781dbddc4f27b62
BeardyCamillus <beardy.camillus@gmail.com>
2023-02-06 21:39:51 +0200

    homework #3 arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -3,6 +3,8 @@ function compareArrays(arr1, arr2) {
 
   // Ваш код
 
+  result = arr1.length === arr2.length && arr1.every((el, index) => el === arr2[index]);
+
   return result; // boolean
 }
 
@@ -10,6 +12,8 @@ function advancedFilter(arr) {
   let resultArr;
 
   // Ваш код
+
+  resultArr = arr.filter(el => el > 0 && el % 3 === 0).map(filteredEl => filteredEl * 10);
 
   return resultArr; // array
 }
